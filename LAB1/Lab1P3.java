4cd9961f67ef374b7b305aabf43d6c8207b591ba
rakshith-eng <muthyamrakshith@gmail.com>
2025-09-03 12:29:13 +0530

    Add files via upload

---
+++ LAB1/Lab1P3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Lab1P3{
+	public static void main(String...args){
+		Scanner sc = new Scanner(System.in);
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			var c = x+y;
+			System.out.println("sum is: "+c);
+		
+	}
+}
