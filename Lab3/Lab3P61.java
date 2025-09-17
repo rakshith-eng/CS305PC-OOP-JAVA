c09ddf44f5f23e86a2f906d404f0ae3ba0976081
rakshith-eng <muthyamrakshith@gmail.com>
2025-09-17 12:49:22 +0530

    Add files via upload

---
+++ Lab3/Lab3P61.java
@@ -0,0 +1,12 @@
+class Lab3P61{
+	String name;
+	public void demonstrate(){
+		System.out.println("i am a method in Lab3P61");
+	}
+	public void setName(){
+		this.name = s;
+	}
+	public String setName(){
+		return this.name;
+	}
+}
---
+++ Lab3/Lab3P62.java
@@ -0,0 +1,10 @@
+class Lab3P62 extends Lab3P61{
+	@override
+	public void demonstrate(){
+		System.out.println("i am a method in Lab3P62");
+	}
+	Lab3P62(){
+		super.demonstrate();
+	}
+
+}
