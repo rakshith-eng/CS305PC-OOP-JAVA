9a46fc765a76ac0cec3a4a0737da6093787a3782
rakshith-eng <muthyamrakshith@gmail.com>
2025-10-15 12:32:15 +0530

    Add files via upload

---
+++ Lab5/Lab5P3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Add{
+        public void add(int...a){
+            String str = " ";
+            int sum = 0;
+            int count = 1;
+                for(int i : a){
+                     sum = sum + i;
+                     System.out.print(i);
+                     if(count!=(a.length)){
+                            System.out.print("+");
+                     }
+                     count++;
+                }
+            System.out.println("="+sum);
+        }    
+}
